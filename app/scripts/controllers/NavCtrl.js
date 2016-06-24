6f756009010d210424109bc7850cb0c624f9fa5c
Carl Truong <Carl@tabrific.com>
2016-06-23 19:26:36 -0600

    Fix nav bar functionality

--- app/scripts/controllers/NavCtrl.js
+++ app/scripts/controllers/NavCtrl.js
@@ -20,6 +20,35 @@
 			nav.loggedIn = User.loggedIn();
 		};
 		
+		nav.navSignUpModal = function() {
+			var modalInstance = $uibModal.open({
+				templateUrl: '/templates/sign_up.html',
+				controller: function($scope, $uibModalInstance) {
+					$scope.newUser = {};
+					$scope.cancel = function() {
+						$uibModalInstance.dismiss('cancel');
+					};
+					$scope.create = function() {
+						$uibModalInstance.close($scope.newUser); 
+					};
+				},
+				size: 'md'
+			});
+			
+			modalInstance.result.then(function(data) {
+				User.createUser(data, function(error, userData){
+					if (error) {
+						console.log("Error creating user:", error);
+						Alert.addAlert("danger", "Whoops, your inputs are wrong, please try again!");
+					} else {
+						console.log("Successfully created user account with uid:", userData.uid);
+						Alert.addAlert("success", "Radical, welcome to the gang. Please log in!");
+					}
+					$scope.$apply();
+				});
+			});
+		};
+		
 		nav.navLogInModal = function() {
 			var modalInstance = $uibModal.open({
 				templateUrl: '/templates/log_in.html',
